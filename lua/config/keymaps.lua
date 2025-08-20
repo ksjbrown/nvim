c184aee8108e30d3e94da4040025c96bdbea705c
Keegan Brown <keeganbrown@outlook.de>
2025-08-20 23:39:02 +0200

    Initial LazyVim based config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+require("snacks").toggle.option("list", { name = "Whitespace" }):map("<leader>uW")
---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.ai_cmp = false
+vim.opt.swapfile = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorlineopt = "number"
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 2
+vim.opt.list = false
+vim.opt.listchars = { tab = "→ ", space = "·", trail = "•", eol = "↴" }
+vim.opt.guicursor = "n-v-c:block-blinkon500-blinkoff500-blinkwait500,i-ci:ver25-blinkon500-blinkoff500-blinkwait500"

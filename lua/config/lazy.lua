b2bab677df3fdf2847936c7b01f7f47bef590fdf
Keegan Brown <keeganbrown@outlook.de>
2025-08-21 09:09:59 +0200

    move plugins.lua to config dir
    For consistency with all other files, move single file plugins spec map
    to config directory

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,23 +1,23 @@
 require("config/bootstrap")
-local plugins = require("ksj/plugins")
+local plugins = require("config/plugins")
 local opts = {
-  ui = {
-    border = "rounded",
-  },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	ui = {
+		border = "rounded",
+	},
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 }
 require("lazy").setup(plugins, opts)

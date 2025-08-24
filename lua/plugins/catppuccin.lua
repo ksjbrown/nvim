ec8c1655f815dfc5879fa024ff25c320bf4057f4
Keegan Brown <keeganbrown@outlook.de>
2025-08-24 21:20:46 +0200

    formatting for catppuccin, rename file to match actual theme name

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -9,13 +9,7 @@ return {
 	priority = 1000,
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
-		local groups = {
-			"NormalFloat",
-			"Pmenu",
-			"PmenuThumb",
-			"Normal",
-		}
-
+		local groups = { "NormalFloat", "Pmenu", "PmenuThumb", "Normal" }
 		local function apply_highlights()
 			for _, group in ipairs(groups) do
 				vim.api.nvim_set_hl(0, group, { bg = "NONE" })
@@ -24,11 +18,7 @@ return {
 			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", fg = "#6c6783" })
 			vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#575268", fg = "#cdd6f4" })
 		end
-
-		-- apply once at startup
 		apply_highlights()
-
-		-- reapply after colorscheme changes
 		vim.api.nvim_create_autocmd("ColorScheme", {
 			pattern = "catppuccin",
 			callback = apply_highlights,
